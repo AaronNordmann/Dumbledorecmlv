7c3fff1f9e42c03ff196475109c4da9a6f8f481b
AaronNordmann <39969289+AaronNordmann@users.noreply.github.com>
2021-03-03 16:33:08 +0100

    Update application-questions.js

--- application-questions.js
+++ application-questions.js
@@ -3,7 +3,6 @@ module.exports =  [
 	"**Vos anciens pseudos InGame** ?",
 	"**Votre niveau In-Game et votre ancienneté* ?",
 	"**Sanctionné récemment In-Game ?\nSi oui, indiquez par qui et pour quel motif, puis, la durée de celle-ci.:**",
-	"**Sanctionné récemment In-Game ?\nSi oui, indiquez par qui et pour quel motif, puis, la durée de celle-ci.:**",
 	"**Occupez vous un poste de leader (entreprise/faction)**?",
 	"**Mon pseudo sur Discord est sous la forme Prénom_Nom** ?",
 	"**J'ai lu et accepté le réglement du Discord**?"
