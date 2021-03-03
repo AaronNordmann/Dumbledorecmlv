e556f4c1d67db975335b6b823ebb921bc804c94f
AaronNordmann <39969289+AaronNordmann@users.noreply.github.com>
2021-03-03 17:03:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -364,7 +364,7 @@ const applicationFormCompleted = (data) => {
 
     const logMessage = {
         embed: {
-            title: `${Community_Tag} | Vérification de ${data.user.tag} - ${data.user.username} - ${msg.author.id}`,
+            title: `${Community_Tag} | Vérification de ${data.user.tag} - ${data.user.username} - ${data.user.id}`,
             color: embedColor,
             fields: [
                 { name: 'Veuillez procéder à la vérification de la demande:\n', value: answers, inline: true },
