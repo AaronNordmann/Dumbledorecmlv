831c1932120baccf319a505cb799284cc01bc587
AaronNordmann <39969289+AaronNordmann@users.noreply.github.com>
2021-03-03 16:36:02 +0100

    Update application-questions.js

--- application-questions.js
+++ application-questions.js
@@ -1,9 +1,9 @@
 module.exports =  [
 	"**Prénom_Nom InGame (il faut être inscrit)** ?",
 	"**Vos anciens pseudos InGame** ?",
-	"**Votre niveau In-Game et votre ancienneté* ?",
-	"**Sanctionné récemment In-Game ?\nSi oui, indiquez par qui et pour quel motif, puis, la durée de celle-ci.:**",
-	"**Occupez vous un poste de leader (entreprise/faction)**?",
+	"**Votre niveau In-Game et votre ancienneté** ?",
+	"**Sanctionné récemment In-Game ?\nSi oui, indiquez par qui et pour quel motif, puis, la durée de celle-ci.** :",
+	"**Occupez vous un poste de leader (entreprise/faction)** ?",
 	"**Mon pseudo sur Discord est sous la forme Prénom_Nom** ?",
-	"**J'ai lu et accepté le réglement du Discord**?"
+	"**J'ai lu et accepté le réglement du Discord** ?"
 ];
